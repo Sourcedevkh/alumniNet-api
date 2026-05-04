e09ab733e7d2a94f89dd00339132818c21593482
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-05 00:55:01 +0700

    validation getme

--- routes/admin/userRoutes.js
+++ routes/admin/userRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const AdminController = require('../../controllers/admin/authController');
+const validate = require('../../middlewares/validate');
+const {loginUserSchema} = require('../../validators/user')
+const { isLogin } = require('../../middlewares/auth');
 
-router.post('/login', AdminController.login);
-router.get('/me/:id', AdminController.getMe);
+router.post('/login', validate(loginUserSchema), AdminController.login);
+router.get('/me', isLogin, AdminController.getMe);
 router.get('/verify-email', AdminController.verifyEmail);
 
 module.exports = router;
