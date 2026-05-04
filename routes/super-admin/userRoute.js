cf7e73b5d2eed3cddc8d991eb642b6bbfa0fdd49
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-04 16:58:19 +0700

    add middlewaer

--- routes/super-admin/userRoute.js
+++ routes/super-admin/userRoute.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const SuperAdminController = require('../../controllers/super-admin/authController');
+const validate = require('../../middlewares/validate');
+const { createUserSchema } = require('../../validators/user')
 
-router.post('/register', SuperAdminController.register);
+router.post('/register', validate(createUserSchema), SuperAdminController.register);
 router.put('/admin-status/:id', SuperAdminController.changeStatus);
 
 module.exports = router;
