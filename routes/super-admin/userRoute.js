372999209a88ff2ac80764760d4dabe5603ef152
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-04 18:48:15 +0700

    add: implement reset password route for super-admin

--- routes/super-admin/userRoute.js
+++ routes/super-admin/userRoute.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const SuperAdminController = require('../../controllers/super-admin/authController');
 const validate = require('../../middlewares/validate');
-const { createUserSchema } = require('../../validators/user')
+const { createUserSchema, resetPasswordSchema } = require('../../validators/user')
 
 router.post('/register', validate(createUserSchema), SuperAdminController.register);
 router.put('/admin-status/:id', SuperAdminController.changeStatus);
+router.put('/reset-pwd/:id', validate(resetPasswordSchema), SuperAdminController.resetPassword);
 
 module.exports = router;
