96e577a802b2f43ca03a555c51e68878a78d058d
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-05 21:57:30 +0700

    route forgot password

--- routes/admin/userRoutes.js
+++ routes/admin/userRoutes.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const AdminController = require('../../controllers/admin/authController');
 const validate = require('../../middlewares/validate');
-const {loginUserSchema, resetVerificationLinkSchema} = require('../../validators/user')
+const {loginUserSchema, resetVerificationLinkSchema, emailSchema, verifyOTPSchema, resetPasswordSchema} = require('../../validators/user')
 const { isLogin } = require('../../middlewares/auth');
 
 router.post('/login', validate(loginUserSchema), AdminController.login);
 router.get('/me', isLogin, AdminController.getMe);
 router.get('/verify-email', AdminController.verifyEmail);
 router.post('/resend-verification-link', validate(resetVerificationLinkSchema), AdminController.resendVerificationLink);
-router.post('/resend-verification-link', validate(resetVerificationLinkSchema), AdminController.resendVerificationLink);
 router.delete('/logout', isLogin, AdminController.logout);
+router.post('/forgot-password', validate(emailSchema), AdminController.forgotPWD);
+router.post('/verify-otp', validate(verifyOTPSchema), AdminController.verifyOTP);
+router.post('/reset-password', validate(resetPasswordSchema), AdminController.resetPassword);
 
 module.exports = router;
