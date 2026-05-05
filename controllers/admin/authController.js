3d7e02cf3fbad3646c0c3b5b382c8a88d4765072
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-05 21:56:35 +0700

    handler forgot PWD

--- controllers/admin/authController.js
+++ controllers/admin/authController.js
@@ -51,10 +51,45 @@ const logout = async (req, res) => {
     }
 }
 
+const forgotPWD = async (req, res) => {
+    try {
+        let email = req.validateBody.email;
+        await authService.requestOTP(email);
+        return sendResponse(res, 200, true, 'OTP sent to your email, Please check your inbox');
+    } catch (error) {
+        return sendResponse(res, 400, false, error.message);
+    }
+}
+
+const verifyOTP = async (req, res) => {
+    try {
+        let {email, code} = req.validateBody;
+        const reset_Token = await authService.verifyOTP(email, code);
+        return sendResponse(res, 200, true, 'OTP verified successed', { reset_Token });
+    } catch (error) {
+        return sendResponse(res, 400, false, error.message);
+    }
+}
+
+const resetPassword = async (req, res) => {
+    try {
+        const { token, password } = req.body; 
+        await authService.resetPWD(token, password); 
+
+        return sendResponse(res, 200, true, 'Password reset successed');
+    } catch (error) {
+        return sendResponse(res, 400, false, error.message);
+    }
+}
+
+
 module.exports = {
     login,
     getMe,
     verifyEmail,
     resendVerificationLink,
-    logout
+    logout,
+    forgotPWD,
+    verifyOTP,
+    resetPassword
 };
