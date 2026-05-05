9534fe55a6b954224e35c47248aab8e14f8f6e69
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-05 23:23:10 +0700

    route super-admin

--- routes/super-admin/userRoute.js
+++ routes/super-admin/userRoute.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const SuperAdminController = require('../../controllers/super-admin/authController');
 const validate = require('../../middlewares/validate');
-const { createUserSchema, resetPasswordSchema } = require('../../validators/user')
+const { createUserSchema, resetAdminSchema } = require('../../validators/user')
 
 router.post('/register', validate(createUserSchema), SuperAdminController.register);
 router.put('/admin-status/:id', SuperAdminController.changeStatus);
-router.put('/reset-pwd/:id', validate(resetPasswordSchema), SuperAdminController.resetPassword);
+router.put('/resetAdmin-pwd/:id', validate(resetAdminSchema), SuperAdminController.resetAdminPassword);
 
 module.exports = router;
