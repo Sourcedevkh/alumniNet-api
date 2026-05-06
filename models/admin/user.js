c22ab28ad30c3e95a930f0812e2ad946fe98f93f
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-06 21:52:13 +0700

    update users

--- models/admin/user.js
+++ models/admin/user.js
@@ -1,17 +1,17 @@
 const { pool } = require('../../config/db');
 
 const findByEmail = async (email) => {
-    let [rows] = await pool.query('SELECT id, name, email, password, phone, address, role, is_active, is_verified, otp_code, otp_expires_at FROM users WHERE email = ?', [email]);
+    let [rows] = await pool.query('SELECT id, fullname, email, password, phone, address, role, is_active, is_verified, otp_code, otp_expires_at FROM users WHERE email = ?', [email]);
     return rows;
 }
 
 const findById = async (id) => {
-    let [rows] = await pool.query('SELECT id, name, email, phone, address, role, is_active, is_verified, updated_at FROM users WHERE id = ?',[id]);
+    let [rows] = await pool.query('SELECT id, fullname, email, phone, address, role, is_active, is_verified, updated_at FROM users WHERE id = ?',[id]);
     return rows;
 }
 
 const findByVerificationEmail = async (token) => {
-    let [rows] = await pool.query('SELECT id, name, email, phone, address, role, is_active, token, is_verified, verification_token, verification_expires FROM users WHERE verification_token = ?', [token]);
+    let [rows] = await pool.query('SELECT id, fullname, email, phone, address, role, is_active, token, is_verified, verification_token, verification_expires FROM users WHERE verification_token = ?', [token]);
     return rows;
 }
 
