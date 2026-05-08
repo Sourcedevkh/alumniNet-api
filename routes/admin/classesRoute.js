6258c59e5203305bb33612bf7e1b172049b3814d
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-08 23:09:52 +0700

    Router classes

---
+++ routes/admin/classesRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const ClassesController = require('../../controllers/admin/classesController');
+const { isLogin } = require('../../middlewares/auth');
+const validate = require('../../middlewares/validate');
+const { classSchema } = require('../../validators/classes');
+
+router.post('/create', isLogin, validate(classSchema), ClassesController.createClasses);
+router.get('/getAll', isLogin, ClassesController.getAll);
+router.get('/getClass/:id', isLogin, ClassesController.getClassbyId);
+router.get('/:id', isLogin, ClassesController.findById);
+router.put('/update/:id', isLogin, validate(classSchema), ClassesController.updateClass);
+
+module.exports = router;
