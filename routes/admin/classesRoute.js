1c23f08824d7331346eec2e9c4738be79076f83f
Phearaa <khonchanphearaa@gmail.com>
2026-05-09 14:23:39 +0700

    Add and update status classes and authentication validation (#50)
    * add: status classes
    
    * updated auth
    
    * query status classes
    
    * updated protected rotue
    
    * status service classes
    
    * validate classes schema

--- routes/admin/classesRoute.js
+++ routes/admin/classesRoute.js
@@ -4,12 +4,13 @@ const router = express.Router();
 const ClassesController = require('../../controllers/admin/classesController');
 const { isLogin } = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
-const { classSchema } = require('../../validators/classes');
+const { classSchema, statusSchema } = require('../../validators/classes');
 
 router.post('/create', isLogin, validate(classSchema), ClassesController.createClasses);
 router.get('/getAll', isLogin, ClassesController.getAll);
 router.get('/getClass/:id', isLogin, ClassesController.getClassbyId);
 router.get('/:id', isLogin, ClassesController.findById);
 router.put('/update/:id', isLogin, validate(classSchema), ClassesController.updateClass);
+router.put('/archive/:id', isLogin, validate(statusSchema), ClassesController.archiveClass);
 
 module.exports = router;
