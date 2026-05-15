a547719380239be60d03c34e8fb80266545485ac
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-05-15 16:38:22 +0700

    route track

---
+++ routes/super-admin/trackRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const TrackController = require('../../controllers/super-admin/trackController');
+
+router.get('/getAllUsers', TrackController.getAllUsers);
+router.get('/getUserDevice', TrackController.getUserDevice);
+router.get('/getAllLoginAttempts', TrackController.getAllLoginAttempts);
+router.get('/getAllUserLoginAttempts', TrackController.getAllUserlogAttempts);
+
+module.exports = router;
